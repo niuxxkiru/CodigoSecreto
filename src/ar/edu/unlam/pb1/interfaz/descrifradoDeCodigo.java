7526a4b1a0983191117701f51ae1c9e83a84a92a
Nicole Mata <98786048+niuxxkiru@users.noreply.github.com>
2023-10-17 21:29:17 -0300

    starting project

---
+++ src/ar/edu/unlam/pb1/interfaz/descrifradoDeCodigo.java
@@ -0,0 +1,27 @@
+package ar.edu.unlam.pb1.interfaz;
+
+import java.util.Scanner;
+
+public class descrifradoDeCodigo {
+
+	static Scanner teclado = new Scanner(System.in);
+
+	public static void main(String[] args) {
+//		1- ingresar codigo
+
+		System.out.println("Ingrese los numeritos(String)");
+		String codigoIngresado = teclado.next();
+		System.out.println("\nUsted ingreso este codigo: " + codigoIngresado);
+
+//		2- leer cada digito uno x uno con un bucle 	
+//		65-90 MAYUSC--------97-122 MIN
+//		iteracion cada dos char y cuando sea >99 iteracion cada 3 char
+		for (int i = 0; i < codigoIngresado.length(); i += 2) {
+			if ((i + 1) < codigoIngresado.length()) {
+				String codigoDeADosChar = codigoIngresado.substring(i, i + 2);
+			}
+		}
+
+	}
+
+}
